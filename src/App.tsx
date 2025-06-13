dbbd8f0d1c0ef06719bf804e69e3cf6d3746f1d6
Syed Moinuddin <syed@perssonify.com>
2025-06-13 10:37:04 +0530

    Add contact route to App component for navigation to the Contact page.

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Index />} />
+            <Route path="contact" element={<Contact />} />
 
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
